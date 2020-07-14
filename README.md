de78b4db6eaf25ee4db155b17a2c58d2a3cf73aa
danybb2020 <daniel.balderas@gmail.com>
2020-07-14 16:49:04 -0700

    Uploaded html, js, css and read me files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dailyScheduler
+Daily Scheduler Calendar
+
+Calendar application where the user can write their to do list and save by clicking on the button on the hour line, color changes if activity is in the past, present or future as well as day in the calendar. Information is saved on the local storage 
+
---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$(".saveBtn").on("click",function(){
+    console.log("buttonclick");
+var value= $(this).siblings(".description").val();
+
+var time= $(this).parent().attr("id");
+localStorage.setItem(time,value);
+
+});
+///need a function to change color
+///need an interval to change it periodically 
+///need to tell what time it is 
+///change the day / check acitivties on changing classses with jquery on google
+
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+});
